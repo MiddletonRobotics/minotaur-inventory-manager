e51ef47789c35d4f75838bdea9b407800c80edc3
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-05-17 14:14:18 -0400

    now should be properly setup

--- app/prisma/prisma.ts
+++ app/prisma/prisma.ts
@@ -1,11 +1,7 @@
+import { PrismaClient } from "./generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client/extension";
 
-const pool: PrismaPg = new PrismaPg({ connectionString: process.env.DATABASEURL! });
-const prisma: PrismaClient = new PrismaClient({ adapter: pool })
-
-const globalForPrisma = global as unknown as { prisma: typeof prisma }
-
-if (process.env.NODE_ENV != "production") globalForPrisma.prisma = prisma
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
