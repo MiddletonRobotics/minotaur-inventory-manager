6eaaf22d1d5fa6bee5733e921711df502f5473ec
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-09-10 10:07:26 -0400

    migrations

---
+++ prisma/migrations/20260907021604_management/migration.sql
@@ -0,0 +1,23 @@
+-- AlterTable
+ALTER TABLE "Category" ALTER COLUMN "id" SET DEFAULT floor(random() * 90000000 + 10000000)::int;
+
+-- AlterTable
+ALTER TABLE "InventoryAdjustment" ALTER COLUMN "id" SET DEFAULT floor(random() * 90000000 + 10000000)::int;
+
+-- AlterTable
+ALTER TABLE "Item" ALTER COLUMN "id" SET DEFAULT floor(random() * 90000000 + 10000000)::int;
+
+-- AlterTable
+ALTER TABLE "Project" ALTER COLUMN "id" SET DEFAULT floor(random() * 90000000 + 10000000)::int;
+
+-- AlterTable
+ALTER TABLE "ProjectCheckout" ALTER COLUMN "id" SET DEFAULT floor(random() * 90000000 + 10000000)::int;
+
+-- AlterTable
+ALTER TABLE "StorageLocation" ALTER COLUMN "id" SET DEFAULT floor(random() * 90000000 + 10000000)::int;
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "id" SET DEFAULT floor(random() * 90000000 + 10000000)::int;
+
+-- AlterTable
+ALTER TABLE "Vendor" ALTER COLUMN "id" SET DEFAULT floor(random() * 90000000 + 10000000)::int;
